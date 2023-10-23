75d14e712c55830ef9fb710267f9163d9c9a1bd7
channacy <68759286+channacy@users.noreply.github.com>
2023-10-22 21:09:02 -0400

    display posts in home page

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -3,14 +3,19 @@ import Navbar from "../navbar";
 import {useSelector} from "react-redux";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
-
+import PostsWidget from "scenes/widgets/PostsWidget";
 const HomePage = () =>{
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
     const {_id, picturePath} = useSelector((state) => state.user);
     return(
         <Box>
             <Navbar />
-            <Box width="100%" padding="2rem 6%" display={isNonMobileScreens? "flex":"block"} gap="0.5rem" justifyContent="space-between">
+            <Box 
+                width="100%" 
+                padding="2rem 6%" 
+                display={isNonMobileScreens? "flex":"block"} 
+                gap="0.5rem" 
+                justifyContent="space-between">
                 <Box flexBasis={isNonMobileScreens? "26%": undefined}>
                     <UserWidget userId={_id} picturePath={picturePath} />
                 </Box>
@@ -19,6 +24,7 @@ const HomePage = () =>{
                     mt={isNonMobileScreens? undefined: "2rem"}>
                 </Box>
                 <MyPostWidget picturePath={picturePath} />
+                <PostsWidget userId={_id} />
                 {isNonMobileScreens &&(<Box flexBasis="26%"></Box>)}
             </Box>
         </Box>
